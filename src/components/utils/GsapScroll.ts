7019f44b5e86771982ae59e9cf3ec6336c53416c
Manav <manavmishra321@gmail.com>
2026-09-01 10:35:07 +0530

    Fix avatar and about section overlap with increased spacing and left offset

--- src/components/utils/GsapScroll.ts
+++ src/components/utils/GsapScroll.ts
@@ -83,7 +83,7 @@ export function setCharTimeline(
       tl1
         .fromTo(character.rotation, { y: 0 }, { y: 0.7, duration: 1 }, 0)
         .to(camera.position, { z: 22 }, 0)
-        .fromTo(".character-model", { x: 0 }, { x: "-32%", duration: 1 }, 0)
+        .fromTo(".character-model", { x: 0 }, { x: "-38%", duration: 1 }, 0)
         .to(".landing-container", { opacity: 0, duration: 0.4 }, 0)
         .to(".landing-container", { y: "40%", duration: 0.8 }, 0);
 
@@ -98,7 +98,7 @@ export function setCharTimeline(
         .fromTo(
           ".character-model",
           { pointerEvents: "none" },
-          { pointerEvents: "none", x: "-35%", delay: 2, duration: 5 },
+          { pointerEvents: "none", x: "-40%", delay: 2, duration: 5 },
           0
         )
         .to(character.rotation, { y: 0.92, x: 0.12, delay: 3, duration: 3 }, 0);
